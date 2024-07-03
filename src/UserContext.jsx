b7a72cda161bbf6ee1ad8c6ada90eb8cd8713fe5
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2024-07-04 01:27:57 +0530

    refresh tokens fixed 1

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,12 +1,21 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [username, setUsername] = useState(null);
   const [id, setId] = useState(null);
+
+  useEffect(() => {
+    axios.get("users/profile").then((response) => {
+      setId(response.data._id);
+      setUsername(response.data.username);
+    });
+  }, []);
+
   return (
-    <UserContext.Provider value={{username, setUsername, id, setId}}>
+    <UserContext.Provider value={{ username, setUsername, id, setId }}>
       {children}
     </UserContext.Provider>
   );
